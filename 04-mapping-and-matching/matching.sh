4aa79d71d0756a178e5cc0395a94546c7474a036
wangdianzheng <wangdianzheng@haomo.ai>
2022-03-08 13:55:32 +0800

    更新

---
+++ 04-mapping-and-matching/matching.sh
@@ -0,0 +1,4 @@
+# set up session:
+source install/setup.bash
+# launch mapping:
+roslaunch lidar_localization matching.launch
---
+++ 04-mapping-and-matching/start.sh
@@ -0,0 +1,6 @@
+# set up session:
+source install/setup.bash
+# launch:
+roslaunch lidar_localization mapping.launch
+
+# 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
